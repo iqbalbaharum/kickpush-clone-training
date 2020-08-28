cd5ab265f86e180a35d6a93a734e180d06a05e5d
iqbalbaharum <muhammad.iqbal.baharum@gmail.com>
2020-08-28 17:57:22 +0800

    Add dynamic list to EJS projects

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 443
+const port = 3000
 
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
@@ -9,8 +9,32 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+const projectList = [
+  { 
+    logo: 'https://cdn.shopify.com/shopifycloud/hatchful-web/assets/5d6266e7bbcdb8fb017c10000ead2fd3.png',
+    company: 'Kicap ABC Sdn Bhd',
+    desciption: 'Teaching a school of Life a thing or two',
+    tech: 'Mobile App / Website',
+    category: 'lifestyle'
+  },
+  { 
+    logo: 'https://cdn.shopify.com/shopifycloud/hatchful-web/assets/01273596e4e92b9de8e8a11cd4ed455e.png',
+    company: 'Medicare Sdn Bhd',
+    desciption: 'Teaching a school of Life a thing or two',
+    tech: 'Website',
+    category: 'Medical'
+  },
+  { 
+    logo: 'https://cdn.shopify.com/shopifycloud/hatchful-web/assets/67cbe9b74baf7f893488c5fc426d31eb.png',
+    company: 'GameHub Sdn Bhd',
+    desciption: 'Game for lonely people',
+    tech: 'Game',
+    category: 'entertainment'
+  }
+]
+
 app.get('/projects', (req, res) => {
-  res.render('projects')
+  res.render('projects', { projects: projectList })
 })
 
 
